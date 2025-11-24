aae7fbfb643624c017ebf949c4ffea04c382877b
MichielN19 <Michielmitsjol@gmail.com>
2025-11-24 19:39:07 +0100

    wip bold: checks if bold text is in the line and wraps in b tag

--- src/parser.rs
+++ src/parser.rs
@@ -11,11 +11,46 @@ fn check_header(line: &str) -> String {
     return parse_to_double_tags(&content, &tag);
 }
 
+fn check_italic_or_bold(line: &str) -> String {
+    let chars: Vec<char> = line.chars().collect();
+    let mut i = 0; 
+    let mut start_index = 0;
+    let mut stop_index = 0;
+
+    while i < chars.len() {
+        if i + 1 < chars.len() && chars[i] == '*' && chars[i + 1] == '*' {
+            start_index = i+2;
+            println!("Start BOLD at {i}");
+            i += 2;
+
+            while i + 1 < chars.len() {
+                if chars[i] == '*' && chars[i + 1] == '*' {
+                    stop_index = i;
+                    println!("End BOLD at {i}");
+
+                    i += 1;
+                    break;
+                }
+                i += 1;
+            }
+            continue;
+        }
+        i += 1;
+    }
+
+    let content = &line[start_index..stop_index];
+    return parse_to_double_tags(content, "b");
+}
+
 pub fn parse_line(line: &str) -> String {
     let trimmed = line.trim();
 
-    if trimmed.starts_with("#") {
+    if trimmed.starts_with('#') {
         return check_header(trimmed);
+    }
+
+    if trimmed.contains('*') {
+        return check_italic_or_bold(trimmed);
     }
 
     if trimmed.is_empty() {
